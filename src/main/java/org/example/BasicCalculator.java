8194eb0491ce3423bb34c3db58190f33cafc7528
podgeeire <conorrice@live.co.uk>
2025-02-28 22:34:00 +0000

    Basic calcs

---
+++ src/main/java/org/example/BasicCalculator.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class BasicCalculator {
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+}
