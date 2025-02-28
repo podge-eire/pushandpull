abb991d39998c3be564d66deed21a04f7310738b
podgeeire <conorrice@live.co.uk>
2025-02-28 23:05:53 +0000

    working terminal calculator

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,32 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Feature Branch.  This is what well deploy!");
+        System.out.println("*** AMAZING CALCULATOR ***"+ "\n");
+
+        BasicCalculator calculator = new BasicCalculator();
+        Scanner input = new Scanner(System.in);
+
+        // takes name input from the keyboard
+        System.out.print("Enter calculation type: ");
+        String calcType = input.nextLine();
+
+        // takes number input from the keyboard
+        System.out.print("Enter first number: ");
+        int number = input.nextInt();
+
+        System.out.print("Enter 2nd number: ");
+        int number2 = input.nextInt();
+
+        int result =  calculator.doCalculation(calcType, number, number2);
+
+        System.out.println("You asked to: " + calcType);
+        System.out.println("The result of the calculation is " + result);
+
+        // closes the scanner
+        input.close();
+
     }
 }
